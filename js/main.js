ae141f80b87c454b131c8c81be46fe4bb477f16a
origotou-ms <shennaijiewantian@gmail.com>
2025-06-29 16:08:47 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,107 @@
+// console.log("ZeroPlus Gate");
+
+// console.log(1);
+// console.log("1");
+
+// console.log(1 + 4);
+// console.log(3 - 2);
+// console.log(2 * 2);
+// console.log(6 / 2);
+
+
+// console.log("ZeroPlus" + "Gate");
+
+
+// 変数
+// var zp = "ZeroPlus";
+// console.log(zp);
+
+// var 変数名 = 値;
+
+// let zpg = "ZeroPlus Gate";
+// console.log(zpg);
+
+
+// 再代入
+// var fruit = "りんご";
+// fruit = "バナナ";
+
+// 再定義
+// var fruit = "パイナップル";
+// console.log(fruit);
+
+
+// let fruit = "りんご";
+
+// fruit = "バナナ"; 
+
+// let fruit = "パイナップル";
+// console.log(fruit);
+
+
+// console.log(1200 + 2200); // 3400
+// console.log(3400 + 800); // 4200
+// console.log(4200 + 4100); // 8300
+
+
+// let total = 0;
+
+// total = 1200;
+// total = total + 2200;
+// total = total + 800;
+// total = total + 4100;
+
+// console.log(total);
+
+
+// // 定数
+// const zp = "ZeroPlus";
+
+// console.log(zp + " Gate");
+// console.log(zp + " Career");
+// console.log(zp + " Club");
+
+
+// 比較演算子
+
+// console.log(1 == 1); // true
+// console.log(1 == 2); // false
+
+// console.log(1 != 2); // true
+// console.log(1 != 1); // false
+
+// console.log(1 === 1); // true
+// console.log(1 === 2); // false
+
+// console.log(1 !== 2); // true
+// console.log(1 !== 1); // false
+
+// console.log(1 > 2); // false
+
+// console.log(1 < 2); // true
+
+// console.log(1 >= 1); // true
+
+// console.log(1 <= 2); // true
+
+
+// 論理演算子
+
+// console.log(true && true); // true
+
+// console.log(true || false); // true
+
+// console.log(!true); // false
+
+
+// if文
+
+// let num = 10;
+
+// if (num == 5) {
+//   console.log("正しい");
+// } else if (num < 5) {
+//   console.log("numは5より小さい");
+// } else {
+//   console.log("正しくない");
+// }
